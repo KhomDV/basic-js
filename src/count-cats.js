d3a7d0096bc226ad52903822d6104f737b85c29f
KhomDV <khomdv@mail.ru>
2021-03-16 16:24:16 +0300

    feat: solution task Count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let nCat = 0;
+
+  if ( !Array.isArray(matrix) || matrix.length === 0 ) {return (nCat)};
+
+  matrix.forEach(e => nCat = nCat + e.filter(e => e === "^^").length);
+
+  return (nCat);
+
 };
