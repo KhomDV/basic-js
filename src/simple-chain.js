1e24f5a05a2d20d17998fc24e3dff6d90a7cc42e
KhomDV <KhomDV@mail.ru>
2021-03-22 23:52:28 +0300

    refactor: edit simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    //this.chain.push( (this.chain.length === 0 ? "" : "~~") + `( ${String(value)} )`);
+    this.chain.push(`( ${String(value)} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if ( position !== Number() || !isInteger(position) || position <= 0 || this.chain.length < (position-1) ) {
+      this.chain = [];
+      return false
+    };
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.join("~~");
   }
 };
 
