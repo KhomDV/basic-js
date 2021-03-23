7fbc589ec3255196d8a63b87231f9cd2903cca5b
KhomDV <khomdv@mail.ru>
2021-03-23 10:22:18 +0300

    feat: solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -3,37 +3,28 @@ const CustomError = require("../extensions/custom-error");
 const chainMaker = {
   chain: [],
   getLength() {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
     return this.chain.length;
   },
-  addLink(value) {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-    //this.chain.push( (this.chain.length === 0 ? "" : "~~") + `( ${String(value)} )`);
+  addLink(value = " ") {
     this.chain.push(`( ${String(value)} )`);
     return this;
   },
   removeLink(position) {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-    if ( position !== Number() || !isInteger(position) || position <= 0 || this.chain.length < (position-1) ) {
+    if (!position || typeof position !== 'number' || position < 0 ) {
       this.chain = [];
-      return false
+      throw new Error();
     };
     this.chain.splice(position-1, 1);
     return this;
   },
   reverseChain() {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
     this.chain.reverse();
     return this;
   },
   finishChain() {
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-    return this.chain.join("~~");
+    this.chainStr = this.chain.join("~~");
+    this.chain = [];
+    return this.chainStr;
   }
 };
 
