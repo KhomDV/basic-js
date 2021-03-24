27d89284b8fad529ea458bd4af8afaba61cfe874
KhomDV <khomdv@mail.ru>
2021-03-24 09:51:37 +0300

    feat: solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -14,26 +14,22 @@ class VigenereCipheringMachine {
         for (let i=0; i<message.length; i++) {
             const char = message[i].toUpperCase();
             if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
-              const charKey = key[i_key].toUpperCase();
-              let charCode = (char.charCodeAt(0)-65) + (charKey.charCodeAt(0)-65);
-              if (Math.trunc(charCode / 26) > 0 ) {
-                charCode = (charCode % 26);
-              }
-              strCrypto = strCrypto + String.fromCharCode(charCode + 65);
-              i_key++;
-              if (i_key > key.length-1) {
-                i_key = 0;
-              }
+                const charKey = key[i_key].toUpperCase();
+                let charCode = (char.charCodeAt(0)-65) + (charKey.charCodeAt(0)-65);
+                if (charCode > 25 ) {
+                    charCode = charCode - 26;
+                }
+                strCrypto = strCrypto + String.fromCharCode(charCode + 65);
+                i_key++;
+                if (i_key > key.length-1) {
+                  i_key = 0;
+                }
             } else {
-              strCrypto = strCrypto + message[i];
+                strCrypto = strCrypto + message[i];
             }
         }
-        if (!this.lineCrypt) {
-          strCrypto.split().reverse().join('');
-        }
-        return (strCrypto);
+        return ( this.lineCrypt ? strCrypto : strCrypto.split('').reverse().join('') );
     }
-
 
     decrypt(message, key) {
         if ( !message || !key) throw new Error();
@@ -44,24 +40,21 @@ class VigenereCipheringMachine {
         for (let i=0; i<message.length; i++) {
             const charCrypto = message[i].toUpperCase();
             if (charCrypto.charCodeAt(0) >= 65 && charCrypto.charCodeAt(0) <= 90) {
-              const charKey = key[i_key].toUpperCase();
-              let charCode = (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
-              if ( charCode < 0 ) {
-                charCode = 26 + (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
-              }
-              strDeCrypto = strDeCrypto + String.fromCharCode(charCode + 65);
-              i_key++;
-              if (i_key > key.length-1) {
-                i_key = 0;
-              }
+                const charKey = key[i_key].toUpperCase();
+                let charCode = (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
+                if ( charCode < 0 ) {
+                    charCode = 26 + charCode;
+                }
+                strDeCrypto = strDeCrypto + String.fromCharCode(charCode + 65);
+                i_key++;
+                if (i_key > key.length-1) {
+                    i_key = 0;
+                }
             } else {
-              strDeCrypto = strDeCrypto + message[i];
+                strDeCrypto = strDeCrypto + message[i];
             }
         }
-        if (!this.lineCrypt) {
-          strDeCrypto.split().reverse().join('');
-        }
-        return (strDeCrypto);
+        return ( this.lineCrypt ? strDeCrypto : strDeCrypto.split('').reverse().join('') );
     }
 
 }
